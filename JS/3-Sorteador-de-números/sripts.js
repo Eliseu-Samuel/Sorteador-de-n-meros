ba58b246ee6aa32c3f8b50ca1be86732369991cc
Eliseu Samuel <eliseusamuel3@gmail.com>
2025-11-13 23:40:33 +0100

    Capturando valores dos famulários

--- JS/3-Sorteador-de-números/sripts.js
+++ JS/3-Sorteador-de-números/sripts.js
@@ -1,1 +1,27 @@
-console.log("Testando")
+const quantity = document.getElementById("quantity")
+const firstNumberInterval = document.getElementById("firstInterval")
+const lastNumberInterval = document.getElementById("lastNumberInterval")
+const form = document.querySelector("form")
+
+//Capturando os valores numéricos dos formulários 
+function getInputsValues (input) {
+    input.addEventListener("input", () => {
+        input.value = input.value.replace(/\D/g, "")
+    })
+
+}
+
+
+getInputsValues(quantity)
+getInputsValues(firstNumberInterval)
+getInputsValues(lastNumberInterval)
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let quantidade = Number(quantity.value)
+    let primeiroValor = Number(firstNumberInterval.value)
+    let ultimoValor = Number(lastNumberInterval.value)
+
+    console.log(quantidade, primeiroValor, ultimoValor)
+})
