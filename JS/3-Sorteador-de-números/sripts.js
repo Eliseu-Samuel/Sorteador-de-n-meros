13457ac2558a1af90a5de95b2ab73fbfbc9cfdf4
Eliseu Samuel <eliseusamuel3@gmail.com>
2025-11-19 18:39:00 +0100

    Aplicando animação com js, e removendo a animação estática no css

--- JS/3-Sorteador-de-números/sripts.js
+++ JS/3-Sorteador-de-números/sripts.js
@@ -8,12 +8,14 @@ const formSection = document.querySelector(".content-form-inputs")
 const resultBox = document.querySelector(".show-result")
 
 const btnNovamente = document.querySelector(".sortearDenovo")
+
+const animationContainer = document.querySelector(".animation-result")
+
 //Capturando os valores numéricos dos formulários 
 function getInputsValues (input) {
     input.addEventListener("input", () => {
         input.value = input.value.replace(/\D/g, "")
     })
-
 }
 
 function sorteador(quantidade, valorMin, valorMax){
@@ -28,8 +30,6 @@ function sorteador(quantidade, valorMin, valorMax){
                 continue
             }            
         }
-
-
         //adiciona valor na lista
         listValue.push(valorSorteado)
     }
@@ -37,6 +37,28 @@ function sorteador(quantidade, valorMin, valorMax){
     return listValue
 }
 
+function mostrarValor(lista){
+
+    animationContainer.innerHTML = ""
+
+    lista.forEach((numeroSorteado, index) => {
+        setTimeout(() => {
+            const anim = document.createElement("div")
+            anim.classList.add("animation")
+
+            const bg = document.createElement("div")
+            bg.classList.add("rotating-bg")
+
+            const span = document.createElement("span")
+            span.textContent = numeroSorteado
+
+            anim.appendChild(bg)
+            anim.appendChild(span)
+            animationContainer.appendChild(anim)
+        },index * 5000)
+
+    })
+}
 
 getInputsValues(quantity)
 getInputsValues(minValue)
@@ -49,7 +71,9 @@ form.addEventListener("submit", (e) => {
     let valorMin = Number(minValue.value)
     let valorMax = Number(maxValue.value)
 
-    console.log(sorteador(quantidade, valorMin, valorMax))
+    let listaDeNumeros = sorteador(quantidade, valorMin, valorMax)
+
+    mostrarValor(listaDeNumeros)
 
     //esconde a div dos inputs
     formSection.classList.add("hide")
@@ -66,4 +90,6 @@ form.addEventListener("submit", (e) => {
 btnNovamente.addEventListener("click", () => {
     resultBox.classList.add("hide")
     formSection.classList.remove("hide")
+
+    animationContainer.innerHTML = " "
 })
